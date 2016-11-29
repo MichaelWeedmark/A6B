805075e4d511a879a6065b00826e5d565dffdd43
MichaelWeedmark <michael.weedmark@ocsbstudent.ca>
2016-11-28 20:55:02 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,23 @@
+/*********************************************************************************
+ * 
+ * Made by Michael Weedmark
+ * Made for ICS4U
+ * Assignment 6b
+ * This is a simple game of Yahtzee
+ *
+ ********************************************************************************/
+
+import java.util.*;
+
+public class Main {
+    
+	public static void main(String[] args){
+		
+		System.out.println("Please press 1 to roll your dice");
+		
+		YahtzeeGame game = new YahtzeeGame();
+		game.StartGame();
+		
+	}
+	
+}
